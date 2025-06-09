fb134dafe54cc8fde1e93a1169ff8490b69efb1f
Demian Rikhard <demianrikhard@outlook.com>
2025-06-09 23:12:35 +0300

    Add files via upload

---
+++ HW3.2.py
@@ -0,0 +1,8 @@
+list = []
+print(list)
+
+if len(list) > 0:
+    a = list.pop()
+    list.insert(0, a)
+
+print(list)
---
+++ HW3.3.py
@@ -0,0 +1,13 @@
+list_1 = [1, 2, 3, 4, 5, 6]
+# list_1 = [1, 2, 3]
+# list_1 = [1, 2, 3, 4, 5]
+# list_1 = [1]
+# list_1 = []
+print(list_1)
+
+b_1 = (len(list_1) + 1) // 2
+
+b_3 = list_1[:b_1]
+b_4 = list_1[b_1:]
+
+print(b_3, b_4)
