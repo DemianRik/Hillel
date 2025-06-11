8d70dba82c9e21b916d719a72c11e42f0cb1f826
DemianRik <demianrikhard@outlook.com>
2025-06-11 09:41:59 -1100

    "add files"

--- HW_4.1.py
+++ HW_4.1.py
@@ -1,3 +1,10 @@
-#Написать список якой перемещает ноли в конец списка
-list_1 = [0, 1, 0, 12, 3]
+list_1 = [0, 1, 2, 0, 3, 0, 4]
 print(list_1)
+
+def move_zeros_to_end(lst):
+    non_zeros = [x for x in lst if x != 0]
+    zeros = [0] * (len(lst) - len(non_zeros))
+    return non_zeros + zeros
+
+result = move_zeros_to_end(list_1)
+print(result)
